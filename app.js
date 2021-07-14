cfc7dbb0b121768c110a9af1997438eed3a9896d
“Reactorize” <87221754+Reactorize@users.noreply.github.com>
2021-07-14 14:32:51 +0200

    Commit 01

---
+++ app.js
@@ -0,0 +1,22 @@
+// GET EXPRESS AND CREATE APP
+const express = require('express');
+const app = express();
+const connectDB = require('./database/connect');
+require('dotenv').config();
+// CREATE A GET REQUEST
+app.get('/', (req, res) => {
+    let result = "";
+    result = `<h1>The Heading</h1>`;
+    res.send(result)
+})
+// CONNECT TO DATABASE AND START SERVER
+const port = 3500;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_DB);
+        app.listen(port, console.log(`The server is now running on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
